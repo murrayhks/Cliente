25718a7a601933b97a744fe7226a512fa5c81923
Arthur Murray <arthur.dartora.tds2023@gmail.com>
2025-01-30 15:12:28 -0300

    Cliente.php

---
+++ CLIENTES/modelos/Cliente.php
@@ -0,0 +1,65 @@
+<?php 
+
+abstract class Cliente{
+    protected int $id;
+    protected string $nomeSocial;
+    protected string $email;
+
+    public abstract function getIdentificacao();
+    public abstract function getNroDoc();
+    public abstract function getTipo();
+    /**
+     * Get the value of id
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nomeSocial
+     */
+    public function getNomeSocial(): string
+    {
+        return $this->nomeSocial;
+    }
+
+    /**
+     * Set the value of nomeSocial
+     */
+    public function setNomeSocial(string $nomeSocial): self
+    {
+        $this->nomeSocial = $nomeSocial;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
+?>
