5aee96869dad1b9686946b10881284968c6b2fab
Arthur Murray <arthur.dartora.tds2023@gmail.com>
2025-02-13 08:05:17 -0300

    Pedido.php

--- RESTAURANTE/modelos/Pedido.php
+++ RESTAURANTE/modelos/Pedido.php
@@ -1,15 +1,48 @@
 <?php
-require_once 'modelos/Prato.php';
 
+require_once("Prato.php");
 class Pedido {
-    public string $nomeCliente;
-    public string $nomeGarcom;
-    public Prato $prato;
+    private string $nomeCliente;
+    private string $nomeGarcom;
+    private Prato $prato;
+    public function __toString()
+    {
+        return sprintf("CLIENTE: %s | GARÇOM: %s | PRATO:  %d | %s | %.2f |", $this->getNomeCliente(), $this->getNomeGarcom(), $this->getPrato()->getNumero(), $this->getPrato()->getNome(), $this->getPrato()->getValor());
+    }
 
-    public function __construct($nomeCliente, $nomeGarcom, $prato) {
+    public function getNomeCliente(): string
+    {
+        return $this->nomeCliente;
+    }
+
+    public function setNomeCliente(string $nomeCliente): self
+    {
         $this->nomeCliente = $nomeCliente;
+
+        return $this;
+    }
+
+    public function getNomeGarcom(): string
+    {
+        return $this->nomeGarcom;
+    }
+
+    public function setNomeGarcom(string $nomeGarcom): self
+    {
         $this->nomeGarcom = $nomeGarcom;
+
+        return $this;
+    }
+
+    public function getPrato(): Prato
+    {
+        return $this->prato;
+    }
+
+    public function setPrato(Prato $prato): self
+    {
         $this->prato = $prato;
+
+        return $this;
     }
 }
-?>
