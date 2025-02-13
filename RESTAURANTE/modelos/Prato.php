e80dedb106094df0e3b5a18192b6034576d0b1c2
Arthur Murray <arthur.dartora.tds2023@gmail.com>
2025-02-13 08:05:36 -0300

    Prato.php

--- RESTAURANTE/modelos/Prato.php
+++ RESTAURANTE/modelos/Prato.php
@@ -1,13 +1,54 @@
-<?php
-class Prato {
-    public int $numero;
-    public string $nome;
-    public float $valor;
+<?php 
 
-    public function __construct($numero, $nome, $valor) {
-        $this->numero = $numero;
+class Prato {
+    private int $numero;
+    private string $nome;
+    private float $valor;
+
+    public function __construct($n, $nome, $v){
+        $this->numero = $n;
         $this->nome = $nome;
+        $this->valor = $v;
+    }
+
+    // public function __toString()
+    // {
+    //     return $this->numero . "|" . $this->nome . "|" . $this->valor . "|";
+    // }
+
+    public function getNumero(): int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getValor(): float
+    {
+        return $this->valor;
+    }
+
+    public function setValor(float $valor): self
+    {
         $this->valor = $valor;
+
+        return $this;
     }
 }
-?>
