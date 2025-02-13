d34ddba85968ac6ac9214c5ba5c02a12fb4ed77e
Arthur Murray <arthur.dartora.tds2023@gmail.com>
2025-02-13 08:04:55 -0300

    execucao.php

--- RESTAURANTE/execucao.php
+++ RESTAURANTE/execucao.php
@@ -1,82 +1,101 @@
 <?php
-require_once ('modelos/Prato.php');
-require_once ('modelos/Pedido.php');
+require_once("modelos/Pedido.php");
+require_once("modelos/Prato.php");
 
-$pratos = [
-    new Prato(1, "CAMARÃO Á MILANESA", 110.00),
-    new Prato(2, "PIZZA MARGHERITA", 80.00),
-    new Prato(3, "MACARRÃO À CARBONARA", 60.00),
-    new Prato(4, "BIFE A PARMEGIANA", 75.00),
-    new Prato(5, "RISOTO AO FUNGHI", 70.00)
-];
+function listarPedido($pedidos){
+    if (count($pedidos) > 0) {
+        foreach ($pedidos as $i => $p) {
+            echo $i+1 . "º " . $p->__toString() . "\n";
+        }
+    } else {
+        echo "NENUHM PEDIDO FOI CADASTRADO.";
+    }
+    
+}
+
+function numeroPrato(array $pratos, int $numero){
+    foreach ($pratos as $p) {
+        if ($p->getNumero() == $numero) {
+            return $p;
+        }
+
+    }
+    return null;
+}
+
+$pratos = array (
+    New Prato(1, "CAMARÃO A MILANESA", 110.00),
+    New Prato(2, "PIZZA DE MARGHERITA", 80.00),
+    New Prato(3, "MACARRÃO A CARBONARA", 60.00),
+    New Prato(4, "BIFE A PARMEGIANA", 75.00),
+    New Prato(5, "RISOTO AO FUNGHI", 70.00)
+);
 
 $pedidos = [];
 
-while (true) {
-    echo "\nBONA COMIDA:\n";
-    echo "1 - CADASTRO\n";
-    echo "2 - CANCELAR\n";
-    echo "3 - LISTAR\n";
-    echo "4 - TOTAL DE VENDAR\n";
-    echo "0 - SAIR\n";
-    $opcao = readline("ESCOLHA A OPÇÃO: ");
 
-    if ($opcao == 1) {
-        // CADASTRO
-        $nomeCliente = readline("NOME DO CLIENTE: ");
-        $nomeGarcom = readline("NME DO GARÇOM: ");
-        echo "PRATOS DISPONIVEIS: \n";
-        foreach ($pratos as $prato) {
-            echo "{$prato->numero} - {$prato->nome} - R$ {$prato->valor}\n";
-        }
-        $numeroPrato = readline("NÚMERO DO PRATO : ");
-        $pratoEscolhido = null;
-        foreach ($pratos as $prato) {
-            if ($prato->numero == $numeroPrato) {
-                $pratoEscolhido = $prato;
-                break;
+do {
+    echo "\n\n----------MENU----------\n";
+    echo "1- CADASTRO:\n";
+    echo "2- CANCELAR:\n";
+    echo "3- LISTAR: \n";
+    echo "4- TOTAL DAS VENDAS: \n";
+    echo "0- SAIR\n";
+    echo "------------------------\n";
+    $opcao = readline("INSIRA A OPÇÃO: ");
+
+    echo "\n";
+
+    switch($opcao) {
+        case 1:
+
+            $pedido = New Pedido; 
+
+            $pedido->setNomeCliente(readline("NOME DO CLIENTE: "))->setNomeGarcom(readline("NOME DO GARÇOM: "));
+
+            echo "PRATOS DISPONIVEIS:\n";
+
+            foreach ($pratos as $p) {
+                echo $p->getNumero() . "|" . $p->getNome() . "| R$" . $p->getValor() . "|\n";
             }
-        }
-        if ($pratoEscolhido) {
-            $pedido = new Pedido($nomeCliente, $nomeGarcom, $pratoEscolhido);
-            $pedidos[] = $pedido;
-            echo "PEDIDO CADASTRADO COM SUCESSO !\n";
-        } else {
-            echo "O PRATO NÃO FOI ENCONTRADO.\n";
-        }
-    } elseif ($opcao == 2) {
-        // CANCELAR
-        $index = readline("ÍNDICE DO PEDIDO A SER CANCELADO: ");
-        if (isset($pedidos[$index])) {
-            array_splice($pedidos, $index, 1);
-            echo "PEDIDO CANCELADO COM SUCESSO !\n";
-        } else {
-            echo "ÍNDICE INVALIDADO.\n";
-        }
-    } elseif ($opcao == 3) {
-        // Listar pedidos
-        if (empty($pedidos)) {
-            echo "NENHUM PEDIDO CADASTRADO.\n";
-        } else {
-            foreach ($pedidos as $index => $pedido) {
-                echo "PEDIDO {$index}:\n";
-                echo "CLEINTE: {$pedido->nomeCliente}\n";
-                echo "GARÇOM: {$pedido->nomeGarcom}\n";
-                echo "PRATO: {$pedido->prato->nome} - R$ {$pedido->prato->valor}\n";
+
+            $prato = null;
+            while($prato == null){
+                $numPrato = readline("INFORME O NUMERO DO PRATO: ");
+
+                $prato = numeroPrato($pratos, $numPrato);
+                // print_r($prato);
             }
-        }
-    } elseif ($opcao == 4) {
-        // TOTAL DAS DVENDAS
-        $total = 0;
-        foreach ($pedidos as $pedido) {
-            $total += $pedido->prato->valor;
-        }
-        echo "TOTAL DAS VENDAS : R$ {$total}\n";
-    } elseif ($opcao == 0) {
-        echo "SAINDO...\n";
-        break;
-    } else {
-        echo "OPÇÃO INVALIDADA.\n";
+
+            $pedido->setPrato($prato);
+
+            array_push($pedidos, $pedido);
+            break;
+        case 2:
+            listarPedido($pedidos);
+
+            if (count($pedidos) > 0) {
+                $id = readline("INSIRA O ID DO PEDIDO QUE DESEJA EXLCUIR OU CANCELAR : ");
+
+                if ($id > 0 && $id <= count($pedidos)) {
+                    array_splice($pedidos, $id-1, 1);
+                } else {
+                    echo "ESTE PEDIDO NÃO ESTÁ DISPONIVEL!\n";
+                }
+            } 
+            //array_slice
+            // print_r($pedidos);
+            break;
+        case 3:
+            listarPedido($pedidos);
+            break;
+        case 4: 
+            echo "TOTAL DE PEDIDOS: " . count($pedidos);
+            break;
+        case 0:
+            echo "SAINDO...!\n";
+            break;
+        default:
+            echo "OPÇÃO INVALIDADA!\n";
     }
-}
-?>
+} while($opcao != 0);   
